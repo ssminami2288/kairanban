84bccf11b755275a1595c906ed82ba46266a1d1a
ssminami28 <ssminami28@gmail.com>
2024-05-13 15:27:38 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-$(document).ready(function() {
-  $('.popup-image').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  });
-});
