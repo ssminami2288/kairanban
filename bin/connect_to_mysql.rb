78a7cf17b3f02952a34d817d4c068c3b70add548
ssminami28 <ssminami28@gmail.com>
2024-05-15 23:53:28 +0900

    修正

--- bin/connect_to_mysql.rb
+++ bin/connect_to_mysql.rb
@@ -8,12 +8,70 @@ client = Mysql2::Client.new(
   database: "kairanban_development"  # データベース名
 )
 
-# クエリを実行して結果を取得
-results = client.query("SELECT * FROM table_name")
+tables = client.query("SHOW TABLES")
+table_names = tables.map { |table| table.values.first }
 
-# 結果を表示
-results.each do |row|
-  puts row
+# クエリを実行して結果を取得
+table_names.each do |table_name|
+  results = client.query("SELECT * FROM accesses")
+  puts "Data from table accesses"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM active_storage_attachments")
+  puts "Data from table active_storage_attachments"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM active_storage_blobs")
+  puts "Data from table active_storage_blobs"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM ar_internal_metadata")
+  puts "Data from table ar_internal_metadata"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM meetings")
+  puts "Data from table meetings"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM news")
+  puts "Data from table news"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM posts")
+  puts "Data from table posts"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM schema_migrations")
+  puts "Data from table schema_migrations"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM stamps")
+  puts "Data from table stamps"
+  results.each do |row|
+    puts row.inspect
+  end
+
+  results = client.query("SELECT * FROM users")
+  puts "Data from table users"
+  results.each do |row|
+    puts row.inspect
+  end
 end
 
 # 接続を閉じる
