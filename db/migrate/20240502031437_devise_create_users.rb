39de034e065a83efe14ed6412e984084a30df533
ssminami2288 <ssminami2288@gmail.com>
2024-05-16 18:13:36 +0900

    修正

--- db/migrate/20240502031437_devise_create_users.rb
+++ db/migrate/20240502031437_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string  :email,              null: false,unique: true, default: ""
+      t.string  :email,              null: false, default: ""
       t.string  :encrypted_password, null: false, default: ""
       t.string  :last_name,          null: false
       t.string  :first_name,         null: false
-      t.integer :room_number,        null: false, unique: true
+      t.integer :room_number,        null: false
       t.boolean :admin, default: false
 
       ## Recoverable
