c1621615bf859ed15a0159148d06dd22bfb80b03
ssminami2288 <ssminami2288@gmail.com>
2024-05-20 23:57:43 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,8 +39,8 @@ class PostsController < ApplicationController
     end
   end
 
+
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
