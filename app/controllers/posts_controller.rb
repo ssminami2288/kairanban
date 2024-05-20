b2d6860010dc99ba1a35119b2b5672751c6a11b9
ssminami2288 <ssminami2288@gmail.com>
2024-05-21 00:06:19 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,7 +76,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:date, :title, :text, pdfs: [], images: [])
+    params.require(:post).permit(:date, :title, :text, pdfs:, images:)
   end
 
   def set_posts
