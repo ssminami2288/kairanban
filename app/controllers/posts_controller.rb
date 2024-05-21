008168aa3de86d673bae5cb673875daa757a4c08
ssminami2288 <ssminami2288@gmail.com>
2024-05-21 11:34:08 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,8 +44,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     Rails.logger.debug "Params: #{params.inspect}"
     Rails.logger.debug "Post params: #{post_params.inspect}"
-    if params[:post][:pdfs].blank?
+    if params[:post][:pdfs].blank? && @post.pdfs.attached?
+      existing_pdfs = params[:post][:existing_pdfs]
       if @post.update(post_params.except(:pdfs))
+        @post.pdfs.attach(existing_pdfs.map { |signed_id| ActiveStorage::Blob.find_signed(signed_id) })
         redirect_to post_path(@post)
       else
         render :edit, status: :unprocessable_entity
