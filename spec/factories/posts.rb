61c2ce0fc2805b491ff4a2affc978f718a73d638
ssminami28 <ssminami2288@gmail.com>
2024-05-21 22:36:06 +0900

    テストコード

---
+++ spec/factories/posts.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :post do
+    date { Date.today }
+    title { "Sample Post" }
+    text { "This is a sample post." }
+    trait :with_pdfs do
+      after :create do |post|
+        post.pdfs.attach(io: File.open('path/to/pdf/file.pdf'), filename: 'file.pdf')
+      end
+    end
+  end
+end
